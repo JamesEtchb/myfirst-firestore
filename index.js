c716cd10649d113e22d077ea1cb22d26abb18557
James Etchberger <jamesetchberger@Jamess-MacBook-Air.local>
2022-07-22 15:23:22 -0400

    added a bunch of different ways to put info into the database and then make querys and get info from the database

--- index.js
+++ index.js
@@ -8,7 +8,57 @@ initializeApp({
 
 const db = getFirestore()
 
-const car = { make: "Audi", model: "A3", year: 2018, color: "Red" }
+// const car = { make: "Mercedez", model: "Z3", year: 2020, color: "White" }
+// const car2 = { make: "Macerati", model: "MZ20", year: 2022, color: "Green" }
+// const car3 = { make: "Porsche", model: "911", year: 2022, color: "Black" }
 
-db.collection("cars").add(car)
+//this adds a new file with the info we put but will always make a new file
+// db.collection("cars")
+//   .add(car)
+//   .add(car2)
+//   .add(car3)
+//   .then((doc) => {
+//     console.log("Doc added:", doc.id)
+//   })
+//   .catch(console.error)
 
+//this doesnt add a new doc unless there isnt one previously named that
+//this only changes whats in the document once it exists
+// db.collection('cars').doc('lambo')
+//     .set({make: 'Lamborghini', model: 'Diablo', year: 2020, color: 'Yellow'})
+
+//this how to update a document without erasing any  other of the info
+// db.collection("cars").doc("lambo").update({ color: "red" })
+
+//this is how to get information from the database
+//single document
+db.collection("cars")
+  .doc("lambo")
+  .get()
+  .then((doc) => {
+    console.log(doc.id)
+    console.log(doc.data())
+  })
+  .catch(console.error)
+
+//Get a whole collection
+db.collection("cars")
+  .get()
+  .then((collection) => {
+    collection.docs.forEach((doc) => console.log(doc.id, doc.data()))
+  })
+  .catch(console.error)
+
+//query docs from collection
+db.collection("cars")
+  .where("year", ">", 2015)
+  .get()
+  .then((collection) => {
+    const cars = collection.docs.map((doc) => {
+        let car = doc.data()
+        car.id = doc.id
+        return car
+    })
+    console.log(cars)
+  })
+  .catch(console.error)
